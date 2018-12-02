f89c34d7f2ee833f8f655773346fcab1b60486a1
lominat <lominatgebreselassie27@gmail.com>
2018-12-02 16:48:58 -0600

    lab5

---
+++ Lab5/ex1.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+const {from} = require('rxjs');
+const {map} = require('rxjs/operators');
+
+
+const app = express();
+app.set('trust proxy',true);
+app.set('strict routing',true);
+app.set('x-powered-by',false);
+app.enable('case sensitive routing');
+
+//using prmoises
+
+app.get('/users',(req,resp)=>{
+    axios.get('http://jsonplaceholder.typicode.com/users/').then(
+        data=> resp.send(data.data)
+    ).catch(e=>console.log(e))
+
+})
+let result;
+
+//using Observables
+
+
+app.get('/users',(req,resp)=>{
+    from(axios.get('http://jsonplaceholder.typicode.com/users/'))
+    .pipe(map(data=>data.data))
+    .subscribe(data=>resp.send(data))
+    
+    })
+    app.listen(6000,()=>console.log('server started on port 6000...'))
+    
+//using Aynch and wait
+
+app.get('/users', async(req,resp)=>{
+  
+        try{
+            result = await axios.get('http://jsonplaceholder.typicode.com/users/') ;
+            resp.send(result.data);
+        }catch(error){
+            console.log(error);
+        }  
+})
